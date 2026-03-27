1caaa982aafd156bbb2cd901dd08fffde01fd2c4
Bhupendra Kushwaha <bhupendrakushwahaup95@gmail.com>
2026-03-27 14:39:07 +0530

    Phase 1 Complete: Assignment_Operators using Variables

--- src/Assignment_Operators.rs
+++ src/Assignment_Operators.rs
@@ -1,3 +1,36 @@
 fn main(){
+        // --- Phase 1: Assignment using Variables ---
 
+    // Note: 'mut' is used because we are changing the value
+    let mut x: i32 = 10;
+    let y: i32 = 5;
+    println!("Value of x is : {}", x);
+    println!("Value of y is : {}", y);
+
+    // 1. Simple Assignment (=)
+    x = 20;
+    println!("Simple Assignment (x = 20 ): {}", x);
+
+    // 2. Addition Assignment (x += ) -> x = x + y
+    x += y;
+    println!("Addition Assignment (x += {} ): {}", y, x);
+
+    // 3. Subtraction Assignment (-=) -> x = x-y
+    x -= 2;
+    println!("Subtraction Assignment (x -= 2): {}", x);
+
+    // 4. Multiplication Assignment (*=) -> x = x * Y
+    x *= 3;
+    println!("Multiplication Assignment (x *= 3): {}", x);
+
+    // 5. Division Assignment (/=) -> x = x / y
+    x /= 5;
+    println!("Division Assignment (x /= 5) : {}", x);
+
+    // 6. Remainder Assignment (%=) -> x = x % y
+    x %= 4;
+    println!("Remainder Assignment (x %= 4): {}", x);
+
+    println!("--------------------------------------------------------------------");
+    
 }
